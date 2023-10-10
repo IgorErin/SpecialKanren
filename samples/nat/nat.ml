f2d8fc8879fe922c85155760a89c50fc243305d8
IgorErin <erin.igore@yandex.ru>
2023-10-10 22:14:09 +0300

    refactor: samples/nat

--- samples/nat/nat.ml
+++ samples/nat/nat.ml
@@ -1,6 +1,3 @@
-module L = List
-open GT
-open Printf
 open OCanren
 open OCanren.Std
 open Nat
@@ -12,11 +9,3 @@ let rec le x y is =
       x =/= o &&& (y === o) &&& (Bool.falso === is);
       Fresh.two (fun x' y' -> x === succ x' &&& (y === succ y') &&& le x' y' is);
     ]
-
-let _ =
-  L.iter (fun (x, y) -> printf "x = %s y = %s\n" x y)
-  @@ Stream.take ~n:10
-  @@ run qr
-       (fun x y -> le x y Bool.falso)
-       (fun x y ->
-         (show Nat.logic (x#reify Nat.reify), show Nat.logic (y#reify Nat.reify)))
