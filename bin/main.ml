695f712040546c28094a835051f9dd4f79582f34
IgorErin <erin.igore@yandex.ru>
2023-10-23 23:16:02 +0300

    refactor: read ml files work

--- bin/main.ml
+++ bin/main.ml
@@ -7,37 +7,53 @@ let message = ""
 let options =
   let open SpecialKanren.Config in
   [ "-par", Arg.String set_param, "<parameter> Set output parameter name to <file>"
-  ; "-var", Arg.String set_variant, "<variant> Set variant name to <variant> "
   ; "-fname", Arg.String set_fname, "<fname> Set function name to <fname>"
   ; "-v", Arg.Unit verbose_on, "Set verbose on"
   ]
 ;;
 
 let () = Arg.parse options anon_fun message
+let tool_name = "SpecialKanren"
+let std_lib_names = [ "OCanren" ]
 
-let read_cmt_impl path =
-  if Stdlib.Sys.file_exists path
-  then (
-    let infos = Cmt_format.read_cmt path in
-    infos.Cmt_format.cmt_annots
-    |> function
-    | Cmt_format.Implementation cmt ->
-      if Config.verbose () then Printf.printf "Reading cmt file %s \n%!" path;
-      cmt
-    | _ -> failwith "Implementation expected")
-  else failwith "File does not exist"
+let get_std_lib_pathes () =
+  List.filter_map
+    (fun name ->
+      let open Findlib in
+      try Some (package_directory name) with
+      | No_such_package (lib_name, reason) ->
+        Printf.eprintf
+          "No such std package '%s'%s\n"
+          lib_name
+          (if reason <> "" then Printf.sprintf ", reason: %s" reason else ".");
+        None)
+    std_lib_names
+;;
+
+let read_ml_file input_name =
+  Clflags.include_dirs := List.append (get_std_lib_pathes ()) !Clflags.include_dirs;
+  Clflags.recursive_types := true;
+  Clflags.open_modules := List.append [ "OCanren" ] !Clflags.open_modules;
+  Compmisc.init_path ();
+  let open Compenv in
+  let outputprefix = output_prefix input_name in
+  let modulename = module_of_filename input_name outputprefix in
+  Env.set_unit_name modulename;
+  let env = Compmisc.initial_env () in
+  let { Typedtree.structure = s; _ } =
+    Pparse.parse_implementation ~tool_name input_name
+    |> Typemod.type_implementation input_name outputprefix modulename env
+  in
+  s
 ;;
 
 let () =
   let path = SpecialKanren.Config.path () in
   let par = SpecialKanren.Config.param () in
-  let var = SpecialKanren.Config.variant () in
   let fname = SpecialKanren.Config.fname () in
-  if Config.verbose ()
-  then Printf.printf "path: %s\npar: %s\nvariant: %s\nfname: %s%!" path par var fname;
-  let cmt = read_cmt_impl path in
+  if Config.verbose () then Printf.printf "path: %s\npar: %s\nfname: %s%!" path par fname;
+  let str = read_ml_file path in
   let parp = Predicate.par_of_string par in
-  let varp = Predicate.var_of_string var in
   let funp = Predicate.fun_of_string fname in
-  SpecialKanren.Translator.translate funp parp varp cmt
+  SpecialKanren.Translator.translate funp parp str
 ;;
