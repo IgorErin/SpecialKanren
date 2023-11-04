dc4e015c955b28c3b363f2d242cd71e292c29495
IgorErin <erin.igore@yandex.ru>
2023-11-04 19:38:56 +0300

    add: dnf -> Past without functinos resolving

--- lib/semant.ml
+++ lib/semant.ml
@@ -145,11 +145,169 @@ let reduce_vars global dnf =
   conjs, globals
 ;;
 
+module Helpers = struct
+  open Parsetree
+  open Ast_helper
+
+  let loc = Location.none
+  let create_pat ident = Pat.var @@ Location.mknoloc @@ Ident.name ident
+
+  (* copy past from noCanren *)
+  let create_apply f = function
+    | [] -> f
+    | args ->
+      let args = List.map (fun a -> Asttypes.Nolabel, a) args in
+      (match f.pexp_desc with
+       | Pexp_apply (g, args') -> Exp.apply g (args' @ args)
+       | _ -> Exp.apply f args)
+  ;;
+
+  let create_apply_to_list f arg_list =
+    let loc = f.pexp_loc in
+    let new_arg =
+      List.fold_right (fun x acc -> [%expr [%e x] :: [%e acc]]) arg_list [%expr []]
+    in
+    create_apply f [ new_arg ]
+  ;;
+
+  let create_conj left right = [%expr [%e left] &&& [%e right]]
+
+  let create_disj = function
+    | [] -> failwith "Disjunction needs one or more arguments"
+    | [ x ] -> x
+    | [ x; y ] -> [%expr [%e x] ||| [%e y]]
+    | l -> create_apply_to_list [%expr conde] l
+  ;;
+
+  let create_fun var body = [%expr fun [%p create_pat var] -> [%e body]]
+
+  let create_fun_closer vars body =
+    List.fold_right (fun var acc -> create_fun var acc) vars body
+  ;;
+
+  let create_fresh var body = create_apply [%expr Fresh.one] [ create_fun var body ]
+  let create_inj expr = [%expr !![%e expr]]
+
+  (* end of copy past *)
+
+  let create_apply f e = [%expr [%e f] [%e e]]
+  let create_apply_closer f exps = List.fold_left create_apply f exps
+  let create_unify left right = [%expr [%e left] === [%e right]]
+  let create_disunify left right = [%expr [%e left] =/= [%e right]]
+
+  open Ast_helper
+
+  let lid_of_ident ident =
+    ident
+    |> Ident.name
+    |> fun x -> Longident.Lident x |> fun x -> Location.mkloc x Location.none
+  ;;
+
+  let lid_of_string str = Longident.Lident str |> fun x -> Location.mkloc x Location.none
+
+  let exp_of_value value =
+    let rec create_const desc values =
+      let exps = List.map inside_constr values in
+      (match exps with
+       | [] -> None
+       | _ :: _ -> Some (Exp.tuple exps))
+      |> fun arg ->
+      let lid = desc.Types.cstr_name |> lid_of_string in
+      Exp.construct lid arg
+    and inside_constr = function
+      | Var ident -> lid_of_ident ident |> Exp.ident
+      | Constr (desc, values) -> create_const desc values
+    and loop value =
+      match value with
+      | Var ident -> lid_of_ident ident |> Exp.ident
+      | Constr (desc, values) -> create_const desc values |> create_inj
+    in
+    loop value
+  ;;
+
+  let fresh_closer vars cont =
+    List.fold_right (fun var acc -> create_fresh var acc) vars cont
+  ;;
+
+  let exp_of_ident ident = lid_of_ident ident |> Exp.ident
+
+  let to_past_one = function
+    | DUnify (ident, value) ->
+      let left = exp_of_ident ident in
+      let right = exp_of_value value in
+      create_unify left right
+    | DDisunify (ident, value) ->
+      let left = exp_of_ident ident in
+      let right = exp_of_value value in
+      create_disunify left right
+    | DCall (ident, values) ->
+      let values = List.map exp_of_value values in
+      let lident =
+        Untypeast.lident_of_path ident |> fun x -> Location.mkloc x Location.none
+      in
+      let ident = Exp.ident lident in
+      create_apply_closer ident values
+    | DFresh _ -> failwith "DFresh unexpected for now (TODO)"
+  ;;
+
+  let past_of_conj conj =
+    let to_past_unify ident value =
+      let left = exp_of_ident ident in
+      let right = exp_of_value value in
+      create_unify left right
+    in
+    let to_past_disunify ident value =
+      let left = exp_of_ident ident in
+      let right = exp_of_value value in
+      create_disunify left right
+    in
+    let to_past_call ident values =
+      let values = List.map exp_of_value values in
+      let lident =
+        Untypeast.lident_of_path ident |> fun x -> Location.mkloc x Location.none
+      in
+      let ident = Exp.ident lident in
+      create_apply_closer ident values
+    in
+    let to_past_one = function
+      | DUnify (ident, value) -> to_past_unify ident value
+      | DDisunify (ident, value) -> to_past_disunify ident value
+      | DCall (ident, values) -> to_past_call ident values
+      | DFresh _ -> failwith "DFresh unexpected here (TODO)"
+    in
+    (* copy past but compiler aware *)
+    let to_past_regular current next =
+      match current with
+      | DUnify (ident, value) ->
+        let current = to_past_unify ident value in
+        create_conj current next
+      | DDisunify (ident, value) ->
+        let current = to_past_disunify ident value in
+        create_conj current next
+      | DCall (ident, values) ->
+        let current = to_past_call ident values in
+        create_conj current next
+      | DFresh vars -> fresh_closer vars next
+    in
+    match List.rev conj with
+    | [] -> failwith "Empty conj TODO()"
+    | hd :: tl ->
+      let hd = to_past_one hd in
+      List.fold_right to_past_regular (List.rev tl) hd
+  ;;
+
+  let past_of_dnf dnf = List.map past_of_conj dnf |> create_disj
+end
+
 let process par const exp =
-  let global_vars, tree = Canren.of_tast exp in
+  let globals, tree = Canren.of_tast exp in
   let fresh_vars = Canren.get_declared_fresh_vars @@ Option.get tree in
-  let get_new_var = mk_new_var_fun (global_vars @ fresh_vars) in
+  let get_new_var = mk_new_var_fun (globals @ fresh_vars) in
   let vars = List.init const#arity (fun _ -> get_new_var ()) in
+  let globals =
+    List.map (fun var -> if Ident.same var par#ident then vars else [ var ]) globals
+    |> List.concat
+  in
   let result =
     tree
     |> Option.get
@@ -158,9 +316,13 @@ let process par const exp =
     |> filter_by_cons par#by_ident const
     |> List.map (unwrap_const par#by_ident const vars)
   in
-  let dnf, _ = reduce_vars global_vars result in
+  let dnf, globals = reduce_vars globals result in
   let pfst f ident = Format.fprintf f "%s" @@ Ident.name ident in
   let psnd f value = Format.fprintf f "%s" @@ Value.to_string value in
   Format.fprintf Format.std_formatter "\n%!";
-  pp Format.std_formatter pfst psnd dnf
+  Format.printf "Global :";
+  List.iter (fun x -> Format.printf "%s " @@ Ident.name x) globals;
+  pp Format.std_formatter pfst psnd dnf;
+  let body = Helpers.past_of_dnf dnf in
+  Helpers.create_fun_closer globals body
 ;;
--- lib/translator.ml
+++ lib/translator.ml
@@ -1,5 +1,4 @@
 open Typedtree
-open Ocanren_patterns
 open Sresult
 
 let ident_of_string name =
@@ -33,152 +32,11 @@ exception Not_implemeted of string
 let spec_str_item funp parp varp str_item =
   let open Ast_helper in
   let open Typedtree in
-  let var_variant f s =
-    (parp#by_exp f && varp#this s) || (parp#by_exp s && varp#this f)
-  in
-  let var_another_variant f s =
-    (parp#by_exp f && varp#another s) || (parp#by_exp s && varp#another f)
-  in
   let new_name = funp#name ^ "_" ^ varp#name in
-  let untyp_exp = Untypeast.untype_expression in
-  let spec_exp exp =
-    let rec loop exp : Parsetree.expression sresult =
-      match exp.exp_desc with
-      | Texp_function { param; cases = [ { c_rhs; _ } ]; _ } when parp#by_ident param ->
-        loop c_rhs
-      | Texp_function
-          { arg_label; param = _; cases = [ { c_lhs; c_guard; c_rhs } ]; partial = _ } ->
-        let open Untypeast in
-        let c_lhs = untype_pattern c_lhs in
-        let c_guard = Option.map untype_expression c_guard in
-        let c_rhs = loop c_rhs in
-        (* TODO what in another cases ??? *)
-        (match c_rhs with
-         | Expr c_rhs -> Exp.fun_ arg_label c_guard c_lhs c_rhs |> fun x -> Expr x
-         | _ -> c_rhs)
-      | Texp_function { arg_label = _; param = _; cases; partial = _ } ->
-        List.map
-          (fun { c_lhs; c_guard; c_rhs } ->
-            let open Untypeast in
-            let c_lhs = untype_pattern c_lhs in
-            let c_guard = Option.map untype_expression c_guard in
-            let c_rhs = Sresult.get @@ loop c_rhs in
-            (* TODO get above *)
-            (* seems like we should create wildcard case with assert false *)
-            Exp.case c_lhs ?guard:c_guard c_rhs)
-          cases
-        |> Exp.function_
-        |> Sresult.expr
-        (* conde *)
-      | Texp_apply (hd_exp, args) when is_conde hd_exp ->
-        (match args with
-         | [ (lbf, Some e) ] ->
-           let hd_exp = Untypeast.untype_expression hd_exp in
-           let f next = Exp.apply hd_exp [ lbf, next ] in
-           let loc = Location.none in
-           Sresult.map ~f @@ (loop e |> Sresult.with_default [%expr []])
-         | _ -> assert false)
-      (* (::) list cons. assume disj *)
-      | Texp_construct (_, _, args) when is_list_cons exp ->
-        let fst, snd = Assert.bin args in
-        let fst = loop fst in
-        let snd = loop snd in
-        let f x y =
-          let loc = Location.none in
-          [%expr [%e x] :: [%e y]]
-        in
-        reduce_disj fst snd f
-      (* (|||) disj *)
-      | Texp_apply (hd_exp, args) when is_disj hd_exp ->
-        let fexp, sexp = Assert.bin_args args in
-        let fexp = loop fexp in
-        let sexp = loop sexp in
-        let cons x y =
-          let loc = Location.none in
-          [%expr [%e x] ||| [%e y]]
-        in
-        reduce_disj fexp sexp cons
-        (* (&&&) conj *)
-      | Texp_apply (hd_exp, args) when is_conj hd_exp ->
-        let fexp, sexp = Assert.bin_args args in
-        let fexp = loop fexp in
-        let sexp = loop sexp in
-        let cons x y =
-          let loc = Location.none in
-          [%expr [%e x] &&& [%e y]]
-        in
-        reduce_conj fexp sexp cons
-      (* === *)
-      | Texp_apply (hd_exp, args) when is_unify hd_exp ->
-        let fexp, sexp = Assert.bin_args args in
-        (match () with
-         | () when var_variant fexp sexp -> Empty
-         | () when var_another_variant fexp sexp -> ReduceConj
-         | _ -> Expr (untyp_exp exp))
-      (* =/= *)
-      | Texp_apply (hd_exp, args) when is_nunify hd_exp ->
-        let fexp, sexp = Assert.bin_args args in
-        (match () with
-         | () when var_variant fexp sexp -> ReduceConj
-         | () when var_another_variant fexp sexp -> Empty
-         | _ -> Expr (untyp_exp exp))
-      (* rec call *)
-      | Texp_apply (hd_exp, ls) when funp#exp hd_exp ->
-        let _, arg = List.nth ls parp#number in
-        (match arg with
-         | Some arg ->
-           let app_without_param =
-             let new_args =
-               ls
-               |> List.filteri (fun i _ -> i <> parp#number)
-               |> List.filter_map (fun (lb, vl) ->
-                 Option.map (fun x -> lb, Sresult.get @@ loop x) vl)
-             in
-             let hd = ident_of_string new_name in
-             Exp.apply hd new_args
-           in
-           (match () with
-            | () when parp#by_exp arg || varp#this arg ->
-              (* simply delete argument by number (is it correct???) *)
-              Expr app_without_param
-            | () when varp#another arg ->
-              (* if another variant in place of spec par -> reduce conj *)
-              ReduceConj
-              (* if some_var -> add (some_var === spec_variant) in conj with apply *)
-            | () ->
-              let loc = Location.none in
-              Expr
-                [%expr
-                  [%e app_without_param] &&& ([%e untyp_exp arg] === !!varp#instance)])
-         | None -> raise @@ Not_implemeted "Abstracted over spec paramter.")
-      | Texp_apply (hd, args) when is_fresh hd ->
-        (* TODO if fresh var unif with variant *)
-        let lb, e = Assert.lb_arg args in
-        let f x = Exp.apply (untyp_exp hd) [ lb, x ] in
-        loop e |> Sresult.map ~f
-      | Texp_apply (hd, args) ->
-        let hd = untyp_exp hd in
-        let args =
-          List.filter_map
-            (fun (lb, x) ->
-              Option.bind x (fun x ->
-                loop x |> Sresult.map ~f:(fun x -> lb, x) |> Sresult.to_opt))
-            args
-        in
-        Expr (Exp.apply hd args)
-      (* paramter -> variant *)
-      | _ when parp#by_exp exp -> inj_instance varp
-      | _ -> Expr (untyp_exp exp)
-    in
-    loop exp
-  in
   match str_item.str_desc with
   | Tstr_value (recf, [ vb ]) ->
     let pat = Untypeast.untype_pattern vb.vb_pat |> var_with_name new_name in
-    let () = Semant.process parp varp vb.vb_expr in
-    spec_exp vb.vb_expr
-    |> Sresult.get_with_default exp_failwith
-    |> fun x -> Str.value recf [ Vb.mk pat x ]
+    Semant.process parp varp vb.vb_expr |> fun x -> Str.value recf [ Vb.mk pat x ]
   | Tstr_value _ ->
     (* should be checked at validating *)
     assert false
